e41675b4ce63d238602d4f4a1740c8f9c568a82a
AlexanderHolm96 <ilicem@live.se>
2019-10-29 13:43:08 +0100

    Mina filer

---
+++ AHScripts.js
@@ -0,0 +1,9 @@
+//Hej
+function openForm() {
+    document.getElementById("createActivity").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("createActivity").style.display = "none";
+}
+
