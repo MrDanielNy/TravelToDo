811b6575110ae75c1b92a489384dda230392f289
abrei852 <abbe1334@gmail.com>
2019-10-29 13:10:09 +0100

    Files
    AEScripts.js
    createTravel.html

---
+++ AEScripts.js
@@ -0,0 +1,60 @@
+var LocalStorageUser = (function () {
+
+    function init() {
+        const button = document.getElementById("create-travel");
+        button.addEventListener("click", createUser);
+    }
+
+    function createUser(user) {
+        const firstName = document.getElementById("first-name");
+        const lastName = document.getElementById("last-name");
+        const userName = "(Name: " + firstName.value + " " + lastName.value + ")";
+
+        const cityInput = document.getElementById("city-input");
+        const city = " (Destination: " + cityInput.value + ")";
+
+        const fromDateInput = document.getElementById("from-date");
+        const toDateInput = document.getElementById("to-date");
+        const date = " (Date of travel: " + fromDateInput.value + " - " + toDateInput.value + ")";
+
+        const adultsInput = document.getElementById("number-of-adults");
+        const adults = " (Adults: " + adultsInput.value + ")";
+
+        const minors = document.getElementById("minors").checked;
+        let kids = " "; 
+        
+        if(minors === true){
+            kids = " (Minors: Yes) ";
+        }
+
+        else {
+            kids = " (Minors: No) ";
+        }
+
+        const currencyS = document.getElementById("currency-s");
+        const currencyE = document.getElementById("currency-e");
+        const currencyU = document.getElementById("currency-u");
+        let currency = " "
+
+        if(currencyS.checked === true){
+            currency = currencyS.value
+        }
+        else if(currencyE.checked === true){
+            currency = currencyE.value
+        }
+        else if(currencyU.checked === true){
+            currency = currencyU.value
+        }
+
+        money = document.getElementById("spending-money");
+        const spendingMoney = " (Spendings: " + money.value + currency + ")";
+
+        user = userName + city + date + adults + kids + spendingMoney;
+        const userString = JSON.stringify(user);
+        localStorage.setItem("profile", userString);
+    }
+    
+    return { init }
+})();
+
+window.addEventListener("DOMContentLoaded", LocalStorageUser.init);
